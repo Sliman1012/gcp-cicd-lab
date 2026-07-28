276d06ed8d5c2b0ffe27aea5d91b4d12f9fc98c5
Sliman1012 <slimansh1999@gmail.com>
2026-07-28 11:03:55 +0300

    Setup CI/CD pipeline

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>CI/CD Lab - Fresh Start! 🚀</h1><p>Deploying automatically to GCP via GitHub Actions.</p>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
